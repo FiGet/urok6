c9bf0add50313d2f09b6595b821ba419483590de
FiGet <figet91@gmail.com>
2021-09-22 23:09:12 +0300

    urok5

--- src/global/Figet5_9.java
+++ src/global/Figet5_9.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 public class Figet5_9 {
     public static void main(String[] args) {
         //задание 9
+        int k = 0;
         if (args.length == 0) {
             System.out.println("Вы не передали аргументы");
         } else {
             for (String arg : args) {
                 int var = Integer.parseInt(arg);
                 String str = String.valueOf(var);
-                System.out.println(str);
+                k = k + var;
             }
+            System.out.println(k);
         }
     }
 }
