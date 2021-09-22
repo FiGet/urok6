49ee8f1772492a2e4f925e9c19749ec4fa288468
FiGet <figet91@gmail.com>
2021-09-22 22:37:46 +0300

    urok5

--- src/global/Figet5_10.java
+++ src/global/Figet5_10.java
@@ -9,10 +9,11 @@ public class Figet5_10 {
         System.out.print("Введите количество столбцов двумерного массива ");
         int number2 = scanner.nextInt();
         int[][] array = new int[number1][number2];
-
+        int k = 0;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-
+                array[i][j] = array[i][j] + k + 1;
+                k = array[i][j];
                 System.out.print("" + array[i][j] + "");
             }
             System.out.println();
