ad4ac5f46eae88277b624d049aa238a65918d13f
FiGet <figet91@gmail.com>
2021-09-26 00:06:45 +0300

    urok6

--- src/global/Figet6_7.java
+++ src/global/Figet6_7.java
@@ -1,16 +1,23 @@
 package global;
 import java.util.Scanner;
 public class Figet6_7 {
-    //задание 7
+    public static int number;
+    public static int[] array;
+        //задание 7
     public static void main(String[] args) {
+        newarray();
         createarray();
         printarray();
+
     }
-        static void createarray () {
+        static void newarray () {
             Scanner scanner = new Scanner(System.in);
             System.out.print("Введите размерность одномерного массива ");
             int number = scanner.nextInt();
             int[] array = new int[number];
+        }
+        static void createarray() {
+            Scanner scanner = new Scanner(System.in);
             for (int i = 0; i < number; i++) {
                 System.out.println("Введите число");
                 int aNumber = scanner.nextInt();
